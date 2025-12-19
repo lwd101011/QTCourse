1d8cecc95cf633dbf8fbad505a81ed6cdfbd93ac
lwd101011 <466712445@qq.com>
2025-12-19 20:39:31 +0800

    患者信息的添加和修改

--- lab3/lab3/patientview.cpp
+++ lab3/lab3/patientview.cpp
@@ -27,7 +27,8 @@ PatientView::~PatientView()
 
 void PatientView::on_btAdd_clicked()
 {
-    emit goPatientEditView();
+    int currow = IDatabase::getInstance().addNewPatient();
+    emit goPatientEditView(currow);
 }
 
 
@@ -46,6 +47,9 @@ void PatientView::on_btDelete_clicked()
 
 void PatientView::on_btEdit_clicked()
 {
+    QModelIndex curIndex =
+        IDatabase::getInstance().thePatientSelection->currentIndex();
 
+    emit goPatientEditView(curIndex.row());
 }
 
